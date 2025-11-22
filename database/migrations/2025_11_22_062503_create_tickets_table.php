7635adae5e43cce59db9bf248df97ecc633825f9
rnratika . <nuratikabintiardi@gmail.com>
2025-11-22 14:30:13 +0800

    feat: konfigurasi database dan inisialisasi migration awal

---
+++ database/migrations/2025_11_22_062503_create_tickets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id')->constrained('events')->onDelete('cascade');
+            $table->string('name'); // Contoh: VIP, Regular
+            $table->decimal('price', 10, 2);
+            $table->integer('quota');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
